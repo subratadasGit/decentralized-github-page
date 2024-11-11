df4991a34bff82dc5b9194b8140c584d2ab7fa36
Subrata Das <subratadasgit786@gmail.com>
2024-11-12 01:41:43 +0530

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-'use client';
 
-import {PrivyProvider} from '@privy-io/react-auth';
+
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
